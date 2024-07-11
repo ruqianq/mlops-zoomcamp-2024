04f73dd3a362a37be37446d2bbff3098dc182f91
Daisy Lin <dlin@captechventures.com>
2024-07-11 16:10:57 -0400

    mage stuff

--- 07-project/orchestration/default_repo/transformers/encoder.py
+++ 07-project/orchestration/default_repo/transformers/encoder.py
@@ -3,9 +3,12 @@ if "transformer" not in globals():
 if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
 
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 
 @transformer
-def transform(data, *args, **kwargs):
+def transform(df, *args, **kwargs):
     """
     Template code for a transformer block.
 
@@ -19,9 +22,20 @@ def transform(data, *args, **kwargs):
     Returns:
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
-    # Specify your transformation logic here
+    columns_to_dummy = [
+        "VisitFrequency",
+        "PreferredCuisine",
+        "TimeOfVisit",
+        "DiningOccasion",
+    ]
+    columns_to_encode = ["Gender", "MealType"]
+    df = pd.get_dummies(df, columns=columns_to_dummy, dtype=int, drop_first=True)
+    labelencoder = LabelEncoder()
 
-    return data
+    for col in columns_to_encode:
+        df[col] = labelencoder.fit_transform(df[col])
+
+    return df
 
 
 @test
