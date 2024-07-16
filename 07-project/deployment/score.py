6f62467ac3f69e2986e395d9ac2d52cf375db734
Daisy Lin <dlin@captechventures.com>
2024-07-16 16:25:46 -0400

    chore: Update pickle dump in customer-satisfaction.ipynb and read_data function in score.py
    This commit updates the pickle dump in the customer-satisfaction.ipynb notebook to use the 'model' variable instead of 'model_rf'. It also updates the read_data function in the score.py file to handle different file formats and perform additional data preprocessing steps.
    
    These changes improve the accuracy and performance of the customer satisfaction prediction model by using the correct model variable and enhancing the data preprocessing process.

--- 07-project/deployment/score.py
+++ 07-project/deployment/score.py
@@ -2,45 +2,81 @@
 # coding: utf-8
 import pickle
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from flask import Flask, request, jsonify
 
 
-def read_data(filename, categorical):
-    df = pd.read_parquet(filename)
+def read_data(filename):
+    if filename.endswith(".csv"):
+        df = pd.read_csv(filename)
 
-    df["duration"] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-    df["duration"] = df.duration.dt.total_seconds() / 60
+    elif filename.endswith(".parquet"):
+        df = pd.read_parquet(filename)
+    df["Gender"] = df.Gender.astype("category")
+    df["VisitFrequency"] = df.VisitFrequency.astype("category")
+    df["PreferredCuisine"] = df.PreferredCuisine.astype("category")
+    df["TimeOfVisit"] = df.TimeOfVisit.astype("category")
+    df["DiningOccasion"] = df.DiningOccasion.astype("category")
+    df["MealType"] = df.MealType.astype("category")
+    df["DiningOccasion"] = df.DiningOccasion.astype("category")
+    df["Income_per_AverageSpend"] = df["Income"] / df["AverageSpend"]
+    df["AverageSpend_per_GroupSize"] = df["AverageSpend"] / df["GroupSize"]
+    df["Income_per_GroupSize"] = df["Income"] / df["GroupSize"]
 
-    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+    for col in df.columns:
+        if df[col].dtype != "category":
+            df[col] = df[col].astype(float)
 
-    df[categorical] = df[categorical].fillna(-1).astype("int").astype("str")
+    columns_to_dummy = [
+        "VisitFrequency",
+        "PreferredCuisine",
+        "TimeOfVisit",
+        "DiningOccasion",
+    ]
+    columns_to_encode = ["Gender", "MealType"]
+    df = pd.get_dummies(df, columns=columns_to_dummy, dtype=int, drop_first=True)
+    labelencoder = LabelEncoder()
+
+    for col in columns_to_encode:
+        df[col] = labelencoder.fit_transform(df[col])
 
     return df
 
 
-def predict(model_file, df, categorical):
+def predict(model_file, df):
     with open(model_file, "rb") as f_in:
         dv, model = pickle.load(f_in)
-    dicts = df[categorical].to_dict(orient="records")
-    X_val = dv.transform(dicts)
-    return model.predict(X_val)
+    num_columns = [
+        "Age",
+        "Income",
+        "AverageSpend",
+        "GroupSize",
+        "WaitTime",
+        "ServiceRating",
+        "FoodRating",
+        "AmbianceRating",
+    ]
+    scaler = StandardScaler()
+    x_scaled = scaler.fit_transform(df[num_columns])
+    df[num_columns] = pd.DataFrame(x_scaled, columns=num_columns)
+    return model.predict(x_scaled)
 
 
 def save_results(df, y_pred):
     df_result = pd.DataFrame()
-    df_result["ride_id"] = df["ride_id"]
-    df_result["predicted_duration"] = y_pred
-    return df_result.loc[:, "predicted_duration"].mean()
+    df_result["CustomerID"] = df["CustomerID"]
+    df_result["predicted_rating"] = y_pred
+    return df_result.loc[:, "predicted_rating"].mean()
 
 
-app = Flask("duration-prediction")
+app = Flask("customer-rating-prediction")
 
 
 @app.route("/score", methods=["POST"])
 def predict_endpoint():
     categorical = ["PULocationID", "DOLocationID"]
     df = read_data(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-05.parquet",
+        f"s3://customer-satisfaction-823124982163/predict/restaurant_customer_satisfaction 2.csv",
         categorical=categorical,
     )
     y_pred = predict("model.bin", df, categorical=categorical)
